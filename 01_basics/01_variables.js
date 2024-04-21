c01932a1bd0215988bc0856d8d80e7900f768f0f
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-21 19:19:05 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId=144553
+let accountEmail="Amitku.ae@gmail.com"
+var accountPassoword="12345"
+accountCity="Jaipur"
+
+
+// accountId=2.    not allowed
+
+accountEmail="hx@hc.com";
+accountPassoword="212121";
+accountCity="Bijnor";
+let accuntState;
+
+/*
+
+Prefer not to use var
+bacause of issue in block scope and functional scope
+
+*/
+
+
+
+
+console.table([accountEmail,accountId,accountCity,accuntState])
+console.log(accountId);
