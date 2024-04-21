94e874d4f6f41a53b52f44bb7bf67c6a4969ddec
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-21 19:36:39 +0000

    container

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all Js code as newer version
+// alert(3+3). we are using nodejs not browser
+
+let name="Amit"
+let age=18
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof("Amit"))
+console.log(typeof null)
+console.log(typeof undefined)
