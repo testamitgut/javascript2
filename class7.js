1607bd4b99f2469b8ddce0b0ba2f0c8c4a56cbfa
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-23 23:27:03 +0530

    Add files via upload

---
+++ class7.js
@@ -0,0 +1,71 @@
+// let score="33abc";
+// let score=null;
+// let score=undefined;
+// let score=true;
+let score="Amit";
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1,false=>0
+// let isLoggedIn=1;
+// let isLoggedIn=""
+let isLoggedIn="Amit"
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true ;0=>false
+// """=>false
+// "Amit"=>true
+
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+//   *******************Operations ************   
+      
+let value =3;
+let negValue=-value;
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);   
+
+
+let str1="hello" ;
+let str2="Amit" ;
+let str3=str1+str2;
+console.log(str3);
+
+
+   console.log("1"+2);
+   console.log(1+"2");
+   console.log('1'+"2");
+   console.log("1"+2+2)
+   console.log(1+2+"2");
+
+//    console.log(3+4*5%3)
+
+
+// console.log(+true);
+// console.log(+"")
+
+   let num1,num2,num3
+   num1=num2=num3=2+2;           
+   let gameCounter=100
+   gameCounter++;
+   ++gameCounter;
+   console.log(gameCounter);
+//    ++gameCounter;
+   console.log(gameCounter);
