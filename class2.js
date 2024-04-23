8581ee8c8de34882651b63f41a57b1ce806746c6
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-23 22:35:32 +0530

    Add files via upload

---
+++ class2.js
@@ -0,0 +1,24 @@
+"use strict"//treat all Js code as newer version
+// alert(3+3) . we are using nodejs not browser
+console.log(3+3) 
+let name="Amit";
+let age=18;
+let isLoggedIn=false;
+// number =>2 to power 53
+//bigint
+
+//string=>""
+//boolen=>true and false
+
+//null => standalone value
+
+//undefined=>
+//symbol=>unique 
+
+
+
+
+//object 
+
+console.log(typeof undefined)
+console.log(typeof null)
