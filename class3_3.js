32605aa040d24e4ad83fd82a4ff0d930e9aa438b
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 16:32:51 +0530

    Add files via upload

---
+++ class3_3.js
@@ -0,0 +1,45 @@
+let fristName="Amit"
+let lastName="Kumar";
+let age=25;
+fristName="sumit";
+// fristName=100;
+
+let isHappy=true;
+
+let uerIntro='Hi, My name is '
++fristName+' '+lastName+' . I am ' + age + ' '+ 'year old';
+console.log(uerIntro);
+console.log(typeof uerIntro);
+
+let a;
+a='Amit'
+console.log(typeof a);
+let c=null;
+console.log(typeof c);
+// console.log( typeof fristName);
+
+// fristName="sumit";
+
+
+const hoursInDay=24;
+console.log(hoursInDay);
+// hoursInDay=54;
+
+
+var username="Amit";
+console.log(username);
+var b;
+{
+    var userEmail="Amitku.ar@gmai.com";
+}
+console.log(userEmail);
+
+// {
+    // let v=100;
+// }
+// console.log(v);
+
+{
+    const e=100;
+}
+console.log(e);
