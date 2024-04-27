bbc057c2ecd610cdfc1b7add0e5d50d0496cf62e
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-28 02:21:30 +0530

    Add files via upload

---
+++ class5_5.js
@@ -0,0 +1,44 @@
+console.log(13%2);
+console.log(15%4)
+console.log(5**4)
+console.log(Math.PI)
+console.log(Math.SQRT)
+
+console.log(Math.sqrt(16))
+console.log(Math.sqrt(25))
+console.log(Math.pow(2,6));
+console.log(Math.floor(2.4));
+console.log(Math.floor(3.9999999999))
+console.log(Math.floor(-2.4));
+console.log(Math.ceil(2.2));
+console.log(Math.ceil(-2.2))
+
+
+console.log(Math.round(2.2));
+console.log(Math.round(2.5))
+console.log(Math.round(-2.6))
+
+console.log(Math.random())
+
+console.log(Math.random()*20);
+
+console.log(Math.floor(Math.random()*21));
+
+console.log(8/0)
+console.log(typeof Infinity);
+
+// const width= Number(prompt('Enter the rectangle width'));
+// const hieght=Number(prompt('Entre the rectangle hieght'));
+ 
+// console.log(width*hieght);
+// document.write(width+hieght);
+
+
+//Truthy and falsy value 
+
+console.log(Boolean(10));
+console.log(Boolean(NaN))
+console.log(Boolean(0))
+console.log( Boolean(""))
+console.log(Boolean(undefined))
+console.log(Boolean(null))
